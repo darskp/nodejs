8d7dcb4b3cb8b9ff7fedf46a439970efe371c54a
darskp <darshanpatgar100@gmail.com>
2024-03-21 01:18:53 +0530

    HTTP headers

---
+++ 6. HTTP headers in API/index.js
@@ -0,0 +1,28 @@
+//headers - additional info/ meta data about the API request and response
+//  res.setHeader("X-Name","Darshan")
+// add custom header  - prefix should starts with X - - to differentiate with default
+
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/user', (req, res) => {
+    res.setHeader("X-Name","Darshan")
+    console.log(req.headers);
+    return res.json({ status: "USER ROUTER RESPONSE" })
+})
+app.get('/users', (req, res) => {
+    return res.json({ status: "USERS ROUTER RESPONSE" })
+})
+
+app.get('*', (req,res) => {
+    return res.json({ status: "404 Found" })
+})
+
+
+app.listen(PORT, 'localhost', () => {
+    console.log("Listening to the server");
+    console.log(`http://localhost:${PORT}`);
+})
